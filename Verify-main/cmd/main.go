b98964a3147b9dd320a5215b7bf671b6c7a098f2
Krish Singh Hura <95937258+krishsinghhura@users.noreply.github.com>
2025-04-13 14:30:11 +0530

    final

---
+++ Verify-main/cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"verification/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+}
+func main() {
+
+	fmt.Println("Starting server...")
+	r := gin.Default()
+	r.POST("/upload", handlers.UploadHandler)
+
+	r.POST("/verify", handlers.VerifyHandler)
+
+	r.POST("/gemini", handlers.FetchData)
+
+	err := r.Run(":9090")
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+
+}
